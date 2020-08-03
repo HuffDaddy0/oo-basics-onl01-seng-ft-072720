fd497a407df8a0c25e8da423d2a53975dac3ecb6
Tyler Huffman <ty.huff24@gmail.com>
2020-08-03 19:02:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
 
 
+class Book
+  attr_accessor :title, :author_name, :page_count, :genre
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    @page_count += 1
+  end
+end
+
+    
